ddaf7f4ab6c0a69fbe46378704c49120e58dceaf
LST <wer1264@gmail.com>
2021-11-17 15:04:36 +0900

    fix; Post 모델 수정
    writer 추가
    likeUsers 타입 변경

--- src/models/post.js
+++ src/models/post.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
+  writer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   imageUrl: { type: String, default: null },
   contents: { type: String, required: true },
   hashtags: [{ type: String }],
-  likeUsers: [{ type: String }],
-  createdAt: { type: String },
+  likeUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: String, required: true },
 });
 
 const Post = mongoose.model("Post", postSchema);
