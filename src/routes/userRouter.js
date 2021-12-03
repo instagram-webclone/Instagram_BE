9f1997210b76c92eb793f8841711916da6153261
bonggu <poseson92@naver.com>
2021-12-03 22:58:12 +0900

    fix: follow,follower 라우터 추가

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,9 +2,18 @@ const express = require("express");
 
 const { getOwnerPost } = require("../controllers/userController");
 const { isAuth } = require("../middlewares/authMiddleware");
+const {
+  getfollowers,
+  getfollowing,
+  follow,
+} = require("../controllers/followController");
 
 const router = express.Router();
 
 router.get("/:id", isAuth, getOwnerPost);
+
+router.get("/followers/:id", isAuth, getfollowers);
+router.get("/following/:id", isAuth, getfollowing);
+router.put("/follow/:id", isAuth, follow);
 
 module.exports = router;
