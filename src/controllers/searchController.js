f5376642aff5aca9dcb8b7e4051d845a0e35e869
LST <wer1264@gmail.com>
2021-12-04 18:04:36 +0900

    feat: 검색결과 보여주는 기능 구현

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const Post = require("../models/post");
+const mongoose = require("mongoose");
 
 exports.search = async (req, res, next) => {
   const { keyword } = req.query;
@@ -49,6 +50,90 @@ exports.search = async (req, res, next) => {
       },
       { userId: 1, introdution: 1 }
     );
+    return res.status(200).json({ ok: true, user });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+exports.showData = async (req, res, next) => {
+  const {
+    userId,
+    query: { keyword },
+  } = req;
+  try {
+    if (keyword[0] === "#") {
+      const hashKeyword = keyword.split("#")[1];
+      const posts = await Post.find(
+        {
+          hashtags: { $elemMatch: { $regex: new RegExp(hashKeyword, "i") } },
+        },
+        { imageUrl: 1, commentCount: 1, likeCount: 1 }
+      );
+      return res.status(200).json({ ok: true, posts });
+    }
+    let userKeyword = keyword;
+    if (keyword[0] === "@") {
+      userKeyword = keyword.split("@")[1];
+    }
+    // 사용자 검색
+    const owner = await User.findOne({ userId: userKeyword }, { _id: 1 });
+    const user = await User.aggregate([
+      { $match: { userId: userKeyword } },
+      {
+        $lookup: {
+          from: "posts",
+          let: { id: "$_id" },
+          pipeline: [
+            {
+              $match: { $expr: { $eq: ["$writer", "$$id"] } },
+            },
+          ],
+          as: "posts",
+        },
+      },
+      {
+        $project: {
+          name: 1,
+          userId: 1,
+          introdution: 1,
+          website: 1,
+          totalPost: { $size: "$posts" },
+          totalFollow: { $size: "$follow" },
+          totalFollower: { $size: "$follower" },
+          isFollow: {
+            $in: [new mongoose.Types.ObjectId(userId), "$follower"],
+          },
+        },
+      },
+    ]);
+    const post = await Post.aggregate([
+      { $match: { writer: owner._id } },
+      {
+        $project: {
+          imageUrl: 1,
+          commentCount: { $size: "$comments" },
+          likeCount: { $size: "$likeUsers" },
+        },
+      },
+      { $sort: { createdAt: -1 } },
+    ]);
+    // 로그인한 사용자의 정보를 조회하는 경우
+    if (userId === owner._id.toString()) {
+      const userWithSavedPost = await User.findOne(
+        { userId: userKeyword },
+        {
+          savedPost: 1,
+        }
+      ).populate("savedPost", { imageUrl: 1, commentCount: 1, likeCount: 1 });
+      const savedPost = userWithSavedPost.savedPost;
+      return res.status(200).json({ ok: true, user, post, savedPost });
+    } else {
+      return res.status(200).json({ ok: true, user, post });
+    }
     return res.status(200).json({ ok: true, user });
   } catch (error) {
     if (!error.statusCode) {
--- src/routes/searchRouter.js
+++ src/routes/searchRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { isAuth } = require("../middlewares/authMiddleware");
-const { search } = require("../controllers/searchController");
+const { search, showData } = require("../controllers/searchController");
 
 const router = express.Router();
 
 router.get("/search", isAuth, search);
+router.get("/searchResult", isAuth, showData);
 
 module.exports = router;
