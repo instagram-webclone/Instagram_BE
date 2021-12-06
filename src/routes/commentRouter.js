f7a4642f09387682368aa8475d009df5376a6e37
LST <wer1264@gmail.com>
2021-12-06 14:37:54 +0900

    fix: 댓글, 대댓글 기능 주소 수정

--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -3,13 +3,15 @@ const express = require("express");
 const { isAuth } = require("../middlewares/authMiddleware");
 const {
   writeComment,
+  writeReplyComment,
   deleteComment,
   commentLikeUnlike,
 } = require("../controllers/commentController");
 
 const router = express.Router();
 
-router.post("/", isAuth, writeComment);
+router.post("/:postId", isAuth, writeComment);
+router.post("/:postId/:commentId", isAuth, writeReplyComment);
 router.delete("/:postId/:commentId", isAuth, deleteComment);
 router.put("/:commentId/like", isAuth, commentLikeUnlike);
 
